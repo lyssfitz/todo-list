5fea39333d72af6eaf4ed84e80b34f6db18de236
Lyss <lyssfitz@outlook.com>
2020-02-22 14:42:21 +1100

    Create update function to save updated entries

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -6,8 +6,10 @@ class ToDoList extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { todos: [] };
+
 		this.create = this.create.bind(this);
 		this.remove = this.remove.bind(this);
+		this.update = this.update.bind(this);
 	}
 
 	create(newToDo) {
@@ -20,9 +22,27 @@ class ToDoList extends Component {
 			todos: this.state.todos.filter(t => t.id !== id),
 		});
 	}
+	update(id, updatedTask) {
+		const updatedTodos = this.state.todos.map(todo => {
+			if (todo.id === id) {
+				return { ...todo, task: updatedTask };
+			}
+			return todo;
+		});
+		this.setState({ todos: updatedTodos });
+	}
+
 	render() {
 		const todos = this.state.todos.map(todo => {
-			return <ToDoItem key={todo.id} id={todo.id} task={todo.task} removeToDo={this.remove} />;
+			return (
+				<ToDoItem
+					key={todo.id}
+					id={todo.id}
+					task={todo.task}
+					removeToDo={this.remove}
+					updateTodo={this.update}
+				/>
+			);
 		});
 		return (
 			<div>
